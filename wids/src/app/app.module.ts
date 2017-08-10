343acb434eb98a080ef7d12394d5a80f3338dd2f
Sandeep Manocha <sandeepmanocha.hcl@gmail.com>
2017-08-10 15:04:26 -0500

    Fixed modile.ts for comp paths

--- wids/src/app/app.module.ts
+++ wids/src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { QuestionlistComponent } from './questionlist/src/app/questionlist/questionlist/questionlist.component';
-import { QuestionsListComponent } from './src/app/questions-list/questions-list.component';
+import { QuestionsListComponent } from './questions-list/questions-list.component';
+import { QuestionComponent } from './questions-list/question/question.component';
+import { AnswersListComponent } from './questions-list/question/answers-list/answers-list.component';
+import { AnswerComponent } from './questions-list/question/answers-list/answer/answer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionlistComponent,
-    QuestionsListComponent
+    QuestionsListComponent,
+    QuestionComponent,
+    AnswersListComponent,
+    AnswerComponent
   ],
   imports: [
     BrowserModule
