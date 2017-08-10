b8e2068878c99a231bc33b8369ea8193d38164c6
Sandeep Manocha <sandeepmanocha.hcl@gmail.com>
2017-08-10 09:26:04 -0500

    Added QuestionListComp

---
+++ wids/src/app/questionlist/questionlist/questionlist.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-questionlist',
+  templateUrl: './questionlist.component.html',
+  styleUrls: ['./questionlist.component.css']
+})
+export class QuestionlistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
