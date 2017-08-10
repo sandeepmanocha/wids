a981842da8b096cc4d9718cf547f7e8c970a9e88
Sandeep Manocha <sandeepmanocha.hcl@gmail.com>
2017-08-10 09:28:19 -0500

    Deleted Comp Folders to recreate

--- wids/src/app/app.module.ts
+++ wids/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionlistComponent } from './questionlist/src/app/questionlist/questionlist/questionlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionlistComponent
   ],
   imports: [
     BrowserModule
